a50cf168725f8d01937b218b0f5bda69bbea93a7
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-07-15 21:48:02 +0200

    Initial setup.

---
+++ HeavyLog/AppView.swift
@@ -0,0 +1,23 @@
+//
+//  AppView.swift
+//  HeavyLog
+//
+//  Created by Sebastian Staszczyk on 15/07/2021.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text("HeavyLog")
+    }
+}
+
+
+// MARK: - Preview
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
--- HeavyLog/HeavyLogApp.swift
+++ HeavyLog/HeavyLogApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HeavyLogApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppView()
         }
     }
 }
