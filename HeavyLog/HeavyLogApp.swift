de44b57cbec09530a3959fcc9abae4773033c587
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2021-07-15 21:35:05 +0200

    Initial Commit

---
+++ HeavyLog/HeavyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeavyLogApp.swift
+//  HeavyLog
+//
+//  Created by Sebastian Staszczyk on 15/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct HeavyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
