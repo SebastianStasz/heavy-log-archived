f9332d2721ca9a79827b550c68e320c058cab7a5
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-07-24 10:35:03 +0200

    Add value indicator square view.

---
+++ HeavyLog/Scenes/RecordsSection/ValueIndicatorSquareView.swift
@@ -0,0 +1,54 @@
+//
+//  ValueIndicatorSquareView.swift
+//  HeavyLog
+//
+//  Created by Sebastian Staszczyk on 24/07/2021.
+//
+
+import SwiftUI
+
+struct ValueIndicatorSquareView: View {
+
+    var value: String
+    var valueColor: Color
+    var squareColor: Color
+
+    var body: some View {
+        Rectangle()
+            .cornerRadius(.cornerRadiusBase)
+            .overlay(valueIndicatorView)
+            .foregroundColor(squareColor)
+            .frame(width: 49, height: 49)
+    }
+
+    var valueIndicatorView: some View {
+        Text(value)
+            .textOutstanding
+            .foregroundColor(valueColor)
+    }
+}
+
+// MARK: - Initializers
+
+extension ValueIndicatorSquareView {
+    init(reps: Reps) {
+        value = reps.indicator
+        valueColor = reps.color
+        squareColor = reps.colorLight
+    }
+}
+
+
+// MARK: - Preview
+
+struct SquareValueIndicatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 20) {
+            ValueIndicatorSquareView(reps: .one)
+            ValueIndicatorSquareView(reps: .three)
+            ValueIndicatorSquareView(reps: .five)
+        }
+        .previewLayout(.sizeThatFits)
+        .padding()
+    }
+}
