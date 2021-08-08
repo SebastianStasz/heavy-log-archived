b64242f8dd53d0b3208475deaf43eb14f9b6f8f9
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-08-08 21:46:13 +0200

    Add APIService

---
+++ HeavyLog/APIProvider.swift
@@ -0,0 +1,14 @@
+//
+//  APIProvider.swift
+//  HeavyLog
+//
+//  Created by Sebastian Staszczyk on 08/08/2021.
+//
+
+import Combine
+import Foundation
+
+protocol APIProvider {
+    func getData<T: Decodable>(from url: URL) -> AnyPublisher<T, AppError>
+    func getData<T: Decodable>(fromFile fileName: String, withExtension fileExtension: String) -> AnyPublisher<T, AppError>
+}
