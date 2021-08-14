9e60c896ed60511a87eec09720c39e12df974703
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-08-14 10:38:22 +0200

    Creating and updating workout entity

--- HeavyLog/CoreData/WorkoutEntity.swift
+++ HeavyLog/CoreData/WorkoutEntity.swift
@@ -13,9 +13,9 @@ import CoreData
 public class WorkoutEntity: NSManagedObject {
 
     @NSManaged private(set) var title: String
+    @NSManaged private(set) var notes: String?
     @NSManaged private(set) var startDate: Date
     @NSManaged private(set) var endDate: Date
-    @NSManaged private(set) var notes: String?
     @NSManaged private var rate_: String
 
     private(set) var rate: WorkoutRate {
@@ -30,6 +30,23 @@ extension WorkoutEntity {
         request.sortDescriptors = []
         request.predicate = nil
         return request
+    }
+
+    @discardableResult
+    static func create(in context: NSManagedObjectContext, data: Workout) -> WorkoutEntity {
+        let workout = WorkoutEntity(context: context)
+        workout.startDate = data.startDate
+        workout.modify(data: data)
+        return workout
+    }
+
+    @discardableResult
+    func modify(data: Workout) -> WorkoutEntity {
+        title = data.title
+        notes = data.notes
+        endDate = data.endDate
+        rate = data.rate
+        return self
     }
 }
 
--- HeavyLogTests/WorkoutEntityTests.swift
+++ HeavyLogTests/WorkoutEntityTests.swift
@@ -10,12 +10,10 @@ import XCTest
 
 class WorkoutEntityTests: XCTestCase {
 
-    private var cancellable: AnyCancellable?
     private var context = PersistenceController.preview.context
 
     override func setUpWithError() throws {
         context.reset()
-        cancellable = nil
         context = PersistenceController.preview.context
     }
 
@@ -24,4 +22,66 @@ class WorkoutEntityTests: XCTestCase {
     }
 
     // MARK: - Tests
+
+    func test_create_workout_entity() throws {
+        // Before creating there should not be any workouts.
+        try workoutRequestShouldReturnElements(0)
+
+        // Create workout entity.
+        let workout = createWorkoutEntity(data: .sample1)
+
+        // After creating there should be one workout.
+        try workoutRequestShouldReturnElements(1)
+
+        // Verify that workout entit data is correct.
+        try verifyWorkoutEntityData(workout, data: .sample1)
+    }
+
+    func test_edit_exercise_entity() throws {
+        // Create workout entity using sample1 data.
+        let workout = createWorkoutEntity(data: .sample1)
+
+        // Modify workout entity using sample2 data.
+        workout.modify(data: .sample2)
+
+        // Verify that data has been changed.
+        try verifyWorkoutEntityData(workout, data: .sample2, startDate: Workout.sample1.startDate)
+    }
+
+    func test_delete_workout_entity() throws {
+        // Create workout entity.
+        let workout = createWorkoutEntity(data: .sample1)
+
+        // Verify that exercworkoutise entity was created.
+        try workoutRequestShouldReturnElements(1)
+
+        // Delete workout entity
+        workout.delete()
+
+        // Verify that workout entity was deleted.
+        try workoutRequestShouldReturnElements(0)
+    }
+}
+
+// MARK: - Steps
+
+extension WorkoutEntityTests {
+
+    private func createWorkoutEntity(data: Workout) -> WorkoutEntity {
+        WorkoutEntity.create(in: context, data: data)
+    }
+
+    private func workoutRequestShouldReturnElements(_ amount: Int) throws {
+        let request = WorkoutEntity.createFetchRequest()
+        let exercises = try! context.fetch(request)
+        XCTAssertEqual(exercises.count, amount)
+    }
+
+    private func verifyWorkoutEntityData(_ workout: WorkoutEntity, data: Workout, startDate: Date? = nil) throws {
+        XCTAssertEqual(workout.title, data.title)
+        XCTAssertEqual(workout.notes, data.notes)
+        XCTAssertEqual(workout.startDate, startDate ?? data.startDate )
+        XCTAssertEqual(workout.endDate, data.endDate)
+        XCTAssertEqual(workout.rate, data.rate)
+    }
 }
