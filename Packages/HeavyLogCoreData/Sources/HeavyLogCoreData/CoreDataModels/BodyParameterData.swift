b5b08b7d255a7a3f50805fa22d0ce1b8db56c1b9
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2021-08-22 00:33:57 +0200

    Add body parameter entity tests

--- Packages/HeavyLogCoreData/Sources/HeavyLogCoreData/CoreDataModels/BodyParameterData.swift
+++ Packages/HeavyLogCoreData/Sources/HeavyLogCoreData/CoreDataModels/BodyParameterData.swift
@@ -12,3 +12,11 @@ public struct BodyParameterData {
     let value: Double
     let date = Date()
 }
+
+// MARK: - Sample Data
+
+public extension BodyParameterData {
+    static let sampleHeight = BodyParameterData(parameter: .height, value: 178)
+    static let sampleWeight = BodyParameterData(parameter: .weight, value: 83)
+    static let sampleChest = BodyParameterData(parameter: .chest, value: 90)
+}
---
+++ Packages/HeavyLogCoreData/Tests/HeavyLogCoreDataTests/BodyParameterEntityTests.swift
@@ -0,0 +1,96 @@
+//
+//  BodyParameterEntityTests.swift
+//  HeavyLogCoreData
+//
+//  Created by Sebastian Staszczyk on 22/08/2021.
+//
+
+import XCTest
+@testable import HeavyLogCoreData
+
+class BodyParameterEntityTests: XCTestCase, CoreDataSteps {
+
+    var context = PersistenceController.previewEmpty.context
+
+    override func setUpWithError() throws {
+        context.reset()
+        context = PersistenceController.previewEmpty.context
+    }
+
+    // MARK: - Tests
+
+    func test_create_body_parameter_entity() throws {
+        // Define body parameter data.
+        let bodyParameterData = BodyParameterData.sampleChest
+
+        // Before creating, there should not be any body parameters.
+        try fetchRequestShouldReturnElements(0, for: BodyParameterEntity.self)
+
+        // Create body parameter entity using defined data.
+        let bodyParameterEntity = createBodyParameterEntity(data: bodyParameterData)
+
+        // After creating, there should be one body parameter entity.
+        try fetchRequestShouldReturnElements(1, for: BodyParameterEntity.self)
+
+        // Verify that body parameter entity data is correct.
+        try verifyBodyParameterEntityData(bodyParameterEntity, data: bodyParameterData)
+
+        // Save context.
+        try saveContext()
+    }
+
+    func test_modify_body_parameter_entity() throws {
+        // Define initial body parameter data.
+        let initialBodyParameterData = BodyParameterData.sampleChest
+
+        // Defined modified body parameter data.
+        let modifiedBodyParameterData = BodyParameterData.sampleHeight
+
+        // Create body parameter entity using initial data.
+        let bodyParameterEntity = createBodyParameterEntity(data: initialBodyParameterData)
+
+        // Modify body parameter entity using modified data.
+        bodyParameterEntity.modify(bodyParameterData: modifiedBodyParameterData)
+
+        // Verify that data has been changed, parameter should be the same.
+        try verifyModifiedBodyParameterEntityData(bodyParameterEntity, initialData: initialBodyParameterData, modifiedData: modifiedBodyParameterData)
+
+        // Save context.
+        try saveContext()
+    }
+
+    func test_delete_body_parameter_entity() throws {
+        // Create body parameter entity using initial data.
+        let bodyParameterEntity = createBodyParameterEntity(data: .sampleChest)
+
+        // Delete body parameter entity.
+        bodyParameterEntity.delete()
+
+        // Verify that body parameter entity was deleted.
+        try fetchRequestShouldReturnElements(0, for: BodyParameterEntity.self)
+
+        // Save context.
+        try saveContext()
+    }
+}
+
+// MARK: - Steps
+
+extension BodyParameterEntityTests {
+
+    private func createBodyParameterEntity(data: BodyParameterData) -> BodyParameterEntity {
+        BodyParameterEntity.create(in: context, bodyParameterData: data)
+    }
+
+    private func verifyBodyParameterEntityData(_ entity: BodyParameterEntity, data: BodyParameterData) throws {
+        XCTAssertEqual(entity.parameter, data.parameter)
+        XCTAssertEqual(entity.value, data.value)
+        XCTAssertEqual(entity.date, data.date)
+    }
+
+    private func verifyModifiedBodyParameterEntityData(_ entity: BodyParameterEntity, initialData: BodyParameterData, modifiedData: BodyParameterData) throws {
+        XCTAssertEqual(entity.parameter, initialData.parameter)
+        XCTAssertEqual(entity.value, modifiedData.value)
+        XCTAssertEqual(entity.date, modifiedData.date)
+    }
+}
